e3ef84f74af35634564d6c3ac7057fefe4214501
Bley <samibanley@gmail.com>
2020-12-04 18:45:00 -0600

    this should work

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,7 @@
-// const giphyFetch = require("giphy/js-fetch-api");
-
-// const gf = new giphyFetch("JSQCmqsSp8XZji7Vb5INMqaEYXzUtDEv");
-
-// const { data } = gf.search("samibanley advent calendar", {sort: "relevant"});
-
-// const image = document.getElementById("gif");
-// image.src = data[0].url;
-
-//javascript, jQuery
-
 const today = new Date().getDate();
-// const dayNum = today.getDate();
-console.log(today);
 
 const xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=samibanley+advent+calendar+day+${today}&api_key=JSQCmqsSp8XZji7Vb5INMqaEYXzUtDEv&sort=relevant`);
-xhr.done(function (data) {
-  console.log("success got data", data);
+xhr.done((result) => {
   const image = document.getElementById("gif");
-  image.src = data.data[0].embed_url;
-  console.log(data.data[0].embed_url);
+  image.src = result.data[0].images.original.url;
 });
