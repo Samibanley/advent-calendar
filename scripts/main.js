b0a4f4312ce67c795d54c1557926e612c3522cc0
Bley <samibanley@gmail.com>
2020-12-04 19:34:10 -0600

    add date barrier

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const today = new Date().getDate();
+const dayNum = today > 25 ? 25 : today;
 
-const xhr = $.get(`https://api.giphy.com/v1/gifs/search?q=samibanley+advent+calendar+day+${today}&api_key=JSQCmqsSp8XZji7Vb5INMqaEYXzUtDEv&sort=relevant`);
+const xhr = $.get(`https://api.giphy.com/v1/gifs/search?q=samibanley+advent+calendar+day+${dayNum}&api_key=JSQCmqsSp8XZji7Vb5INMqaEYXzUtDEv&sort=relevant`);
 xhr.done((result) => {
   const image = document.getElementById("gif");
   image.src = result.data[0].images.original.url;
