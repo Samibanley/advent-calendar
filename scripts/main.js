8d227ea0c1f467dad1dfcbf7e931810ce39ded42
Bley <samibanley@gmail.com>
2020-12-04 18:56:14 -0600

    why wasn't this already https??

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 const today = new Date().getDate();
 
-const xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=samibanley+advent+calendar+day+${today}&api_key=JSQCmqsSp8XZji7Vb5INMqaEYXzUtDEv&sort=relevant`);
+const xhr = $.get(`https://api.giphy.com/v1/gifs/search?q=samibanley+advent+calendar+day+${today}&api_key=JSQCmqsSp8XZji7Vb5INMqaEYXzUtDEv&sort=relevant`);
 xhr.done((result) => {
   const image = document.getElementById("gif");
   image.src = result.data[0].images.original.url;
+  console.log(image.src);
 });
